54ef8b4694ea010f61f9ed55a32b33e53e3406dc
jerem146 <vasquezever146@gmail.com>
2025-05-19 07:29:02 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -60,7 +60,7 @@ global.avatar = 'https://files.catbox.moe/ulac2p.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.gp1 = ''
+global.gp1 = 'https://chat.whatsapp.com/JxV2ZeGQxmB5aeK46JiCWV'
 global.comunidad1 = 'https://chat.whatsapp.com/GFNGD30Q8VnDIA7BUrAzPE'
 global.channel = 'https://whatsapp.com/channel/0029VbADBSTEawdmbuNz9p1d'
 global.channel2 = ''
