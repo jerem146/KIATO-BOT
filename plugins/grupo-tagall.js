941458546128e60fbdf5abe88951208a115d9e87
jerem146 <vasquezever146@gmail.com>
2025-05-19 00:49:54 -0500

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -18,7 +18,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
   const oi = `*» INFO :* ${pesan}`;
   let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname} ≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`;
   for (const mem of participants) {
-    teks += `┊${customEmoji} @${mem.id.split('😍')[0]}\n`;
+    teks += `┊${customEmoji} @${mem.id.split('🍫')[0]}\n`;
   }
   teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
 
