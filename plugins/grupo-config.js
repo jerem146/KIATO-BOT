6285312d49eb2b22a609294451cb3336e1386585
jerem146 <vasquezever146@gmail.com>
2025-05-20 15:12:54 -0500

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,17 +1,6 @@
 let handler = async (m, { conn, command }) => {
-  const emoji = ''
-  const emoji2 = ''
   const isClose = command === 'close' ? 'announcement' : 'not_announcement'
-
   await conn.groupSettingUpdate(m.chat, isClose)
-
-  if (isClose === 'not_announcement') {
-    m.reply(`${emoji}
-  }
-
-  if (isClose === 'announcement') {
-    m.reply(`${emoji2}
-  }
 }
 
 handler.help = ['open', 'close']
