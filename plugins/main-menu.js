58ed20c7c5462e784b2aa89248c7caab4c012faa
jerem146 <vasquezever146@gmail.com>
2025-05-20 10:13:03 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args }) => {
     let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
     
 let txt = `
-╭──〔  〕─╮
+╭──〔𓂀KIATO-FISHER𓂀〕─╮
 ┃ 👤 Usuario : @${userId.split('@')[0]}
 ┃ 🚦 Estado  : ${conn.user.jid == global.conn.user.jid ? 'Bot Principal 🦾' : 'Sub-Bot 🤖'}
 ┃ 🕤 Uptime  : ${uptime}
